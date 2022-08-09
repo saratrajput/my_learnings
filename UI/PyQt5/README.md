6816d84feb0697a4a3cf9f6465f106ed75bee59c
Suraj Pattar <pattarsuraj@gmail.com>
2022-08-09 09:19:36 +0900

    :hammer: Rearrange files.

--- UI/PyQt5/README.md
+++ UI/PyQt5/README.md
@@ -1,15 +1,30 @@
 # Python GUI Programming Recipes using PyQt5
 
-## Instructions
+### Section 1: 
+
+* Install PyQt5
+```
+pip install pyqt5
+```
+
 * Install Qt Designer
 ```
 pip install pyqt5-tools
 ```
 
+### Section 2: Designing Python GUIs with Qt Designer
+* Installing the PyQt5 designer tools.
+* Creating our first GUI using Qt Designer.
+* Adding controls to our GUI with Qt Designer.
+* Using Qt Designer for widget layout.
+* Converting Qt Designer code to Python code.
+
 * Launch Qt Designer
 ```
 designer
 ```
+
+* Run GUI in Qt-Designer by pressing ```Ctrl+R```.
 
 * Convert ```.ui``` file to ```.py``` file
 ```
@@ -21,4 +36,9 @@ pyuic5 -o ./gui_4_gui_to_python_code.py ./gui_4_gui_to_python_code.ui
 pyuic5 -x -o ./gui_4_gui_to_python_code.py ./gui_4_gui_to_python_code.ui 
 ```
 
-* Run GUI in Qt-Designer by pressing ```Ctrl+R```.
+### Section 3: Enhancing the Qt5 GUI Functionality
+* Calling dialogs from the main window.
+* Decoupling Python code from generated UI code.
+* Building a complex GUI with PyQt5.
+* Multi-threading keeps our GUI responsive.
+* Using Drag and Drop within the PyQt5 GUI.
---
+++ UI/PyQt5/section_3/gui_4_calling_dialogs_from_main_window.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file './gui_4_calling_dialogs_from_main_window.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from dialog import Ui_Dialog
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(735, 304)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(30, 20, 581, 211))
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 735, 20))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox.setTitle(_translate("MainWindow", "Dialog"))
+        self.pushButton.setText(_translate("MainWindow", "Call Dialog"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
