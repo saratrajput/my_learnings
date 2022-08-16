8e06c8967f51d97085c91cd5e022e7a312ecb75d
saratrajput <pattarsuraj@gmail.com>
2022-08-17 00:00:19 +0900

    :zap: Working script ready.

--- windows_games/open_window.py
+++ windows_games/open_window.py
@@ -2,5 +2,5 @@ import subprocess
 import win32gui
 
 
-path_to_app = "C:\Program Files (x86)\BlueStacks X\BlueStacks X.exe"
+path_to_app = "C:\Program Files\BlueStacks_nxt\HD-Player.exe"
 proc = subprocess.Popen(path_to_app)
--- windows_games/resize_window.py
+++ windows_games/resize_window.py
@@ -1,7 +1,7 @@
 import win32gui
 
 
-window_name = "Calculator"
+window_name = "BlueStacks App Player"
 window_handle = win32gui.FindWindow(None, window_name)
 print(window_handle)
 x0, y0, x1, y1 = win32gui.GetWindowRect(window_handle)
@@ -10,4 +10,4 @@ h = y1 - y0
 x0 = 0
 y0 = 0
 
-win32gui.MoveWindow(window_handle, x0, y0, 100, 100, True)
+win32gui.MoveWindow(window_handle, x0, y0, 1000, 600, True)
