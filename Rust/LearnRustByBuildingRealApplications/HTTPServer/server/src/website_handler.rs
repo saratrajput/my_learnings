8861e1a4e53b49c0f193e56b72c9a3eec5860479
Suraj Pattar <pattarsuraj@gmail.com>
2023-02-07 10:50:25 +0900

    Rust course Lesson 57 Routing Incoming Requests.

--- Rust/LearnRustByBuildingRealApplications/HTTPServer/server/src/website_handler.rs
+++ Rust/LearnRustByBuildingRealApplications/HTTPServer/server/src/website_handler.rs
@@ -1,10 +1,18 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        // Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
--- Rust/LearnRustByBuildingRealApplications/README.md
+++ Rust/LearnRustByBuildingRealApplications/README.md
@@ -680,3 +680,20 @@ Hello World! My payload includes a trailing CRLF.
 * The convention in Rust is to name the getter after the field without prefixing it with the ```get``` word.
 
 * ```.as_ref()``` converts from ```&Option<T>``` to ```Option<&T>```.
+
+### 57. Routing Incoming Requests
+
+```
+impl Handler for WebsiteHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
+    }
+}
+```
